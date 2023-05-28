e6358ec72d5984e5be7e85565280a44c5a19d46a
padmed <ddarchinian@gmail.com>
2023-05-28 18:29:49 +0400

    Complete step-1 and step-2  of Unicafe exercise

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -4,11 +4,27 @@ const Button = ({ label, handleStat }) => (
   <button onClick={handleStat}>{label}</button>
 );
 
-const DisplayStat = ({ label, reviewCount }) => (
-  <p>
-    {label} {reviewCount}
-  </p>
-);
+const Statistics = ({ stats }) => {
+  const total = stats.good + stats.neutral + stats.bad;
+  let pstvPerc = (stats.good / total) * 100;
+  let average = (stats.bad * -1 + stats.good) / total;
+
+  if (total === 0) {
+    average = 0;
+    pstvPerc = 0;
+  }
+
+  return (
+    <div>
+      <p>good: {stats.good}</p>
+      <p>neutral: {stats.neutral}</p>
+      <p>bad: {stats.bad}</p>
+      <p>all: {total}</p>
+      <p>average: {average}</p>
+      <p>positive: {pstvPerc} %</p>
+    </div>
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -29,9 +45,7 @@ const App = () => {
       </div>
       <div>
         <h1>Statistics</h1>
-        <DisplayStat label="good" reviewCount={good} />
-        <DisplayStat label="neutral" reviewCount={neutral} />
-        <DisplayStat label="bad" reviewCount={bad} />
+        <Statistics stats={{ good: good, neutral: neutral, bad: bad }} />
       </div>
     </>
   );
