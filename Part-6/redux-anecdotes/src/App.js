5622617323ea8962f35ba178751f256c78938129
padmed <ddarchinian@gmail.com>
2023-08-16 13:36:04 +0400

    Seperate logic in its component

--- Part-6/redux-anecdotes/src/App.js
+++ Part-6/redux-anecdotes/src/App.js
@@ -1,28 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
-import { voteAnecdoteAction } from "./reducers/anecdoteReducer";
 import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const anecdotes = useSelector((state) => state);
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
-
-  const vote = (id) => {
-    dispatch(voteAnecdoteAction(id));
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   );
