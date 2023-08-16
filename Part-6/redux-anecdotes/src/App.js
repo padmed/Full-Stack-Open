f5aba11d7753c239baecb22338edd63bb7a15878
padmed <ddarchinian@gmail.com>
2023-08-16 13:26:41 +0400

    Show anecdotes sorted:

--- Part-6/redux-anecdotes/src/App.js
+++ Part-6/redux-anecdotes/src/App.js
@@ -5,8 +5,9 @@ import {
 } from "./reducers/anecdoteReducer";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
   const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
 
   const vote = (id) => {
     dispatch(voteAnecdoteAction(id));
@@ -22,7 +23,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
+      {sortedAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
