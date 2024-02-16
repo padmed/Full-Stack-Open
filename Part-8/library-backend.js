74886e70362d3aa3453f6a521ea63d48b6b1df97
David Darchinian <98668534+padmed@users.noreply.github.com>
2024-02-16 17:05:07 +0000

    Complete 8.3 exercise

--- Part-8/library-backend.js
+++ Part-8/library-backend.js
@@ -106,10 +106,18 @@ const typeDefs = `
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -117,8 +125,13 @@ const resolvers = {
   Query: {
     bookCount: () =>  books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors,
   },
+
+  Author: {
+    bookCount: (root, args) => books.filter((book) => book.author === root.name).length
+  }
 }
 
 const server = new ApolloServer({
