a09e945282130b312a4677b25d0b0aa6505b005c
padmed <ddarchinian@gmail.com>
2024-08-05 19:50:24 +0000

    Complete exercise 9.5

--- Part-9/index.ts
+++ Part-9/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import calculateBmi from "./bmiCalculator";
 
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+
+  if (isNaN(Number(weight)) || isNaN(Number(height)) || !height || !weight) {
+    res.send({
+      error: "malformatted parameters",
+    });
+  }
+
+  const bmi = calculateBmi(Number(weight), Number(height));
+
+  res.send({ height, weight, bmi });
 });
 
 app.listen(4004, () => {
