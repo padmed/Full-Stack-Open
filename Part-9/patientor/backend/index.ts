fadb849885b6baf8dc8eaa31305b3f7a15ebe6e4
padmed <ddarchinian@gmail.com>
2024-08-22 12:17:03 +0000

    Complete exercise 9.9

--- Part-9/patientor/backend/index.ts
+++ Part-9/patientor/backend/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { Request, Response } from "express";
+import CORS from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(CORS());
 
-app.get("/ping", (_req: Request, res: Response) => {
+app.get("/api/ping", (_req: Request, res: Response) => {
   return res.send("pong");
 });
 
-app.listen(4005, () => {
+app.listen(3001, () => {
   console.log("Server is running");
 });
